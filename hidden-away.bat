e3e9bf027efc2b6e5fc2adaffb50928782392a73
antogit-sys <antonioperrgiorgi@gmail.com>
2023-03-15 20:17:01 +0100

    it --> en

--- hidden-away.bat
+++ hidden-away.bat
@@ -7,10 +7,10 @@ color 0a
 echo ===========
 echo hidden away
 echo ===========
-echo [1]nascondi file	
-echo [2]scova file
+echo [1]hide file	
+echo [2]find file
 echo.
 choice /c 12 /n /m "%userprofile% ~ # " 
-if %errorlevel%==1 goto nascondi
-if %errorlevel%==2 goto scova
+if %errorlevel%==1 goto hiden
+if %errorlevel%==2 goto find
 pause
